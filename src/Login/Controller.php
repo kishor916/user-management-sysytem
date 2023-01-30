411e1f40805f56adf900e439f31bb854396c6b64
kishor shrestha <kisshor916@gmail.com>
2023-01-30 16:35:18 +1100

    delet unused files and folders

--- src/Login/Controller.php
+++ src/Login/Controller.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace Sundarban\Project\Login;
-
 
 use http\Header;
 
@@ -9,29 +7,29 @@ class Controller
 {
     public function index(){
 
-
         if(isset($_POST['Login'])){
 
             $username = $_POST['email'];
             $password = $_POST['password'];
-            $model = new Model();
-            $model->checkLogin($username, $password);
+            if(empty($username) || empty($password)){
+                $msg = "Username or password cannot be empty";
+                $view = 'src/Login/view.html.php';
+                include 'src/Template/template.php';
+            } else {
+                $model = new Model();
+                $model->checkLogin($username, $password);
+            }
 
-        }else{
+        } else {
             $view = 'src/Login/view.html.php';
             include 'src/Template/template.php';
         }
 
-
-
     }
-
 
     public function logout(){
-
         session_destroy();
         header('Location: index.php?module=Login');
-
     }
-
 }
+?>
